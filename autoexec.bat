6184d0ea40178b601ccc155311a92e6ee7463484
Rui <lplusplus@gmail>
2022-02-09 20:09:32 -0600

    made scd simpler

--- autoexec.bat
+++ autoexec.bat
@@ -3,19 +3,15 @@
 set UTILS_HOME=C:\utils
 
 REM it seems doskey is not very stable when other processes are trying to
-REM start cmd in hidden mode.
+REM start cmd in hidden mode. So do not set this auto run in registry for
+REM cmd.
+REM 
 REM alias
-rem doskey l=dir $*
-rem doskey cd=%~dp0\scd.bat $*
-rem doskey pn=%UTILS_HOME%\pn\pn.exe $*
-rem doskey npp="C:\Program Files (x86)\Notepad++\notepad++.exe" $*
-rem doskey vsvar="%VS90COMNTOOLS%vsvars32.bat"
-rem doskey ahkw=start /wait %~dp0\ahk-scriptwriter\ahkwriter.exe --console --windowless $*
-rem doskey ahki=%~dp0\ahk-scriptwriter\ahkimagetool.exe $*
-rem doskey ahk=start /wait %UTILS_HOME%\autohotkey\autohotkey.exe $*
-rem doskey alias=doskey /macros
-rem doskey cropborder=java -jar %UTILS_HOME%\cropborder\cropborder.jar
-rem doskey find=C:\cygwin64\bin\find.exe $*
+doskey l=dir $*
+doskey cd=%~dp0\scd.bat $*
+doskey npp="C:\Program Files (x86)\Notepad++\notepad++.exe" $*
+doskey alias=doskey /macros
+doskey find=C:\cygwin64\bin\find.exe $*
 
 rem if exist "C:\Program Files\7-Zip" (
 rem 	doskey 7z="C:\Program Files\7-Zip\7z.exe" $*
@@ -27,5 +23,3 @@ set LESS=-Ri
 
 set CYGWIN=nodosfilewarning
 set PROMPT=$_$P$_$G
-
-set MSBUILD_V4=C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
