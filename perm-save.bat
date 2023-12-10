86523a0c893d2b236cebbb797370906dded2cf91
Rui <lplusplus@gmail>
2023-12-09 22:53:26 -0600

    improved permission save

--- perm-save.bat
+++ perm-save.bat
@@ -5,3 +5,4 @@ icacls "%~1" /save C:\tmp\perms.txt /c
 
 @rem to restore permission settings to a different folder on d:
 @rem icacls . /restore C:\tmp\perms.txt /c
+@rem note: assuming current folder on d: has the same folder name as what is in perms.txt
